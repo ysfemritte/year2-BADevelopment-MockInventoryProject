eaa893155b63930b96a1b7a3ecf8e72e5db743f2
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 18:35:09 +0400

    Add files via upload

---
+++ MockInventoryProject/Inventory_Emritte.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace MockInventoryProject
+{
+    class Inventory_Emritte
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //1. Create 2 different ogjects of class inventory
+                //ClassName objName = new Constructor(parameter list)
+                MockInventory objInv1 = new MockInventory("001", "Pencils", 50, 200.0, 10.0);
+                MockInventory objInv2 = new MockInventory("002", "Copybook", 40, 100.0, 5.0);
+
+
+
+                //2. For each object
+                //2a. Display all values
+                Console.WriteLine($"Item Number: {objInv1.ItemNumber}, Description: {objInv1.Description}, Stock: {objInv1.Stock}, Price: {objInv1.Price}, GST = {objInv1.GST}");
+
+                //Item Number: 002, Description: Copybook, Stock: 40, Price: 100.0, GST: 5.0 
+                Console.WriteLine($"Item Number: {objInv2.ItemNumber}, Description: {objInv2.Description}, Stock: {objInv2.Stock}, Price: {objInv2.Price}, GST = {objInv2.GST}");
+                Console.WriteLine("");
+
+
+                //2b. Call the method for the Total value of stock and display the value 
+                Console.WriteLine("Total Value of Stock of Item 1 is: " + objInv1.Total().ToString());
+                Console.WriteLine("Total Value of Stock of Item 2 is: " + objInv2.Total().ToString());
+                Console.WriteLine("");
+
+
+                //2c. Call the method for the Retail value of an item and display the value
+                Console.WriteLine("Retail Value of Item 1 is: " + objInv1.Retail().ToString());
+                Console.WriteLine("Retail Value of Item 2 is: " + objInv2.Retail().ToString());
+                Console.WriteLine("");
+
+
+
+                //3. For the first object
+                //3a. Decrease the quantity of stock by 1
+                objInv1.Stock = objInv1.Stock - 1;
+
+                //3b. Call the method for the value of stock and display the value.
+                Console.WriteLine("New Total Value of Stock of Item 1 is: " + objInv1.Total().ToString());
+                Console.WriteLine("");
+
+
+
+                //4. For the 2nd object:
+                //4a. Change the item description
+                objInv2.Description = "Exercise Book";
+
+                //4b. Change the price per item
+                objInv2.Price = 120.0;
+
+                //4c. Display the following:
+                //4ci. New item description name
+                //4cii. New Price per item
+                Console.WriteLine($"Item {objInv2.Description}, New Price: {objInv2.Price}");
+
+                //4ciii. the retail value per item
+                Console.WriteLine("New Retail Value per Item 2 is: " + objInv2.Retail().ToString());
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+        }// End Main
+    }
+}
---
+++ MockInventoryProject/MockInventory.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInventoryProject
+{
+    class MockInventory
+    {
+        //1. Create Instance Variable - IV (PRIVATE & LOWER CASE) --> Needs to be implemented in 3. Implemented Properties
+        private int stock;
+        private double price;
+        private double gst;
+
+        //2. Auto-Implemented Properties --> No need to validate - String DType
+        public string ItemNumber { get; set; }
+        public string Description { get; set; }
+
+        //3. Implemented Properties
+        public int Stock
+        {
+            get
+            {
+                //return IV
+                return stock;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    stock = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Stock", value, "should be greater than 0");
+                }
+            }
+        }// end of stock
+
+        public double Price
+        {
+            get
+            {
+                //return IV
+                return price;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Price", value, "should be greater than 0");
+                }
+            }
+        }// end of price
+
+        public double GST
+        {
+            get
+            {
+                //return IV
+                return gst;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 12.0)
+                {
+                    gst = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("GST", value, "should be greater than 0 and less than 12");
+                }
+            }
+        }// end of GST
+
+        //4. Parameterless Constructor
+
+        //5. Parameterised Constructor
+        public MockInventory(string _itemNumber, string _description, int _stock, double _price, double _gst)
+        {
+            //Properties = Parameters
+            ItemNumber = _itemNumber;
+            Description = _description;
+            Stock = _stock;
+            Price = _price;
+            GST = _gst;
+        }
+
+        //6. Methods
+
+        /// <summary>
+        /// Purpose: Determine the total value of stock
+        /// Argument: None
+        /// </summary>
+        /// <returns>Returns the total amount as a Double value</returns>
+        /// 
+        public double Total()
+        {
+            double result;
+            result = stock * price;
+            return result;
+        }
+
+
+        /// <summary>
+        /// Purpose: Determine the retail value of a single item
+        /// Argument: None
+        /// </summary>
+        /// <returns>Returns the retail value as Double</returns>
+        public double Retail()
+        {
+            double retail;
+            retail = Price + (Price * GST);
+            return retail;
+        }
+
+    } //End of class - NO CODES 
+}// End of Namespace
